edde208804802fbbca10f8ac94314e5da54a8cdf
1ampdust <lampdust06@gmail.com>
2024-07-07 14:40:37 +0200

    upd

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -7,7 +7,6 @@ import {
   requestResetToken,
   resetPassword,
 } from '../services/auth.js';
-
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
