a808ab5acf395ea042b4e54207f7e30284da634f
1ampdust <lampdust06@gmail.com>
2024-07-07 12:19:33 +0200

    upd

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 //Setting a port using an environment variable
 const PORT = Number(env('PORT', '3000'));
@@ -33,6 +34,7 @@ export const setupServer = () => {
 
   //upload photos
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   // Logging with pino
   app.use(
